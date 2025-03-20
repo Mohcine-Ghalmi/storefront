03cadca15f9c99ec299570b16d716692388167da
Mohcine-Ghalmi <mohcin.ghalmi@gmail.com>
2025-03-20 04:03:34 +0000

    Update: creating the needed  files for the website plus addinf routers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
+import Contact from "./Components/Contact";
+import Home from "./Components/Home";
+import LoginForm from "./Components/LoginForm";
+import Privacy from "./Components/Privacy"
+import Signup from "./Components/Signup";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <h1>APP</h1>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
